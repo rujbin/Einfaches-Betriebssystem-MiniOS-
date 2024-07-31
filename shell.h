6ac2a6810ec268542c26490a24c28bfad344745f
rujbin <115537871+rujbin@users.noreply.github.com>
2024-07-31 19:19:16 +0200

    Update shell.h

--- shell.h
+++ shell.h
@@ -1,6 +1,13 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+// Hauptfunktion der Shell
 void shell();
+
+// Hilfsfunktionen für die Shell
+void print_string(const char *str);
+void clear_screen();
+void list_files();
+void handle_command(const char *command);
 
 #endif // SHELL_H
